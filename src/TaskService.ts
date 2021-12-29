9750b3b3a758ca450d7b524967e71989dd7f90e0
Kendall Kant <kantkendall@gmail.com>
2021-12-29 16:25:04 -0500

    Task service implementation

--- src/TaskService.ts
+++ src/TaskService.ts
@@ -1,12 +1,24 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import axios from 'axios';
+// const axios = require('axios').default;
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
-  @Cron(CronExpression.EVERY_10_SECONDS)
-  handleCron() {
+  @Cron(CronExpression.EVERY_5_MINUTES)
+  async handleCron() {
     console.log('init task serv');
     this.logger.debug('Called every 30 seconds');
+
+    const ipfsUrl = 'https://api.ancon.did.pa/';
+
+    let ipfsRes: any;
+    axios.get(ipfsUrl + 'v0/proofs/lasthash').then(function (response) {
+      ipfsRes = response;
+      // handle success
+      console.log(response.data);
+    });
+    // console.log(ipfsRes);
   }
 }
