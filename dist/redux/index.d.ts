45d7def2bc9a3baed8e598e0b8c8c36ecb628500
Kendall Kant <kantkendall@gmail.com>
2022-03-18 17:46:02 -0500

    clean dist

--- dist/redux/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class DAGChainReduxHandler {
-    private rules;
-    private from;
-    private anconEndpoint;
-    constructor(rules: any, from: string, anconEndpoint: string);
-    handleEvent(evmEvent: any, topicContent: any, previousIndexContent: any): Promise<any>;
-}
--- dist/utils/DagHelper.d.ts
+++
@@ -1,7 +0,0 @@
-import { ethers } from 'ethers';
-import AnconProtocol from './AnconProtocol';
-export declare const anconPostMetadata: (_address: any, _uuid: string, _web3Prov: ethers.providers.Web3Provider, Ancon: AnconProtocol, _wallet: ethers.Wallet, payload: any) => Promise<any>;
-export declare const anconUpdateMintMetadata: (_address: any, _uuid: string, _web3Prov: ethers.providers.Web3Provider, Ancon: AnconProtocol, _wallet: ethers.Wallet, oldPayload: any, _blockchainTxHash: string, _blockchainTokenId: string, _mintBlockNumber: number) => Promise<any>;
-export declare const anconUpdateMetadataMakeOrder: (_address: any, _uuid: string, _web3Prov: ethers.providers.Web3Provider, Ancon: AnconProtocol, _wallet: ethers.Wallet, oldPayload: any, _blockchainMakeOrderTxHash: string, _currentOrderHash: string, _makeOrderBlockNumber: number, _price: number, _timestamp: number) => Promise<any>;
-export declare const anconUpdateMetadataCancelOrder: (_address: any, _uuid: string, _web3Prov: ethers.providers.Web3Provider, Ancon: AnconProtocol, _wallet: ethers.Wallet, oldPayload: any) => Promise<any>;
-export declare const anconUpdateMetadataClaim: (_address: any, _uuid: string, _web3Prov: ethers.providers.Web3Provider, Ancon: AnconProtocol, _wallet: ethers.Wallet, oldPayload: any, _buyerAddress: string) => Promise<any>;
